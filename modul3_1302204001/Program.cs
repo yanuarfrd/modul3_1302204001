dc1ca7d9735d69d86ea2e485e27ddd45dfd910a4
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-03-12 11:40:02 +0700

    menambahkan class KodeBuah

--- modul3_1302204001/Program.cs
+++ modul3_1302204001/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+class KodeBuah
+{
+    public enum NamaBuah
+    {
+        Apel,
+        Alprikot,
+        Alpukat,
+        Pisang,
+        Paprika,
+        Blackberry,
+        Ceri,
+        Kelapa,
+        Jagung,
+        Kurma,
+        Durian,
+        Anggur,
+        Melon,
+        Semangka
+    };
+
+   
+
+    public static string getKodeBuah(NamaBuah InputBuah)
+    {
+        string[]arrayKodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00","J00", "K00", "L00", "M00", "N00", "O00"};
+        int indexJawaban = (int)InputBuah;
+        return arrayKodeBuah[indexJawaban];
+    }
+}
+
+/////////////////////////////////////////////
+
+class program
+{
+    static void Main(string[] args)
+    {
+        
+           
+            KodeBuah.NamaBuah inputNamaBuah = KodeBuah.NamaBuah.Kurma;
+            Console.WriteLine("Buah dengan Nama:" + inputNamaBuah + "= kode nya adalah:" + KodeBuah.getKodeBuah(inputNamaBuah));
+        
+        
+
+    }
+}
